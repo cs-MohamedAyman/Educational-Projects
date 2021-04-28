707522fa56163c6398e83816c8e5e609edef0201
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-28 22:11:18 +0200

    Update Remembery-Game.py

--- Remembery-Game/Remembery-Game.py
+++ Remembery-Game/Remembery-Game.py
@@ -1,15 +1,15 @@
 import random
-N = 3
-grid = []
+N, M = 4, 4
+viewed_grid, grid = [], []
 
 #This function prints the grid of 8 Puzzle Game as the game progresses
 def print_grid():
-    print('-' * (N * N + N + 1))
+    print('-' * (N * M + M + 1))
     for i in range(N):
         print(end='|')
         for j in range(N):
-            if grid[i][j] == 0:
-                e = ' ' * N
+            if viewed_grid[i][j] == 0:
+                e = '..'.center(N)
             else:
                 str_len = len(str(grid[i][j]))
                 r1 = (N - str_len + 1) // 2
@@ -17,79 +17,89 @@ def print_grid():
                 e = (' ' * r1) + str(grid[i][j]) + (' ' * r2)
             print(e, end='|')
         print()
-        print('-' * (N * N + N + 1))
-
-#This function generates cells in the given grid
-def generate_grid():
-    for i in range(1, N*N):
-        a = random.randint(0, N-1)
-        b = random.randint(0, N-1)
-        while grid[a][b] != 0:
-            a = random.randint(0, N-1)
-            b = random.randint(0, N-1)
-        grid[a][b] = i
+        print('-' * (N * M + M + 1))
 
 #This function checks if the game state reachs the win state or not 
 def check_win():
-    idx = 1
     for i in range(N):
-        for j in range(N):
-            if grid[i][j] != idx % (N*N):
+        for j in range(M):
+            if viewed_grid[i][j] == 0:
                 return False
-            idx += 1
     return True
 
-#This function checks if given cell is empty or not 
-def check_empty(i, j):
-	return grid[i][j] == 0
+#This function generates cells in the given grid
+def generate_grid():
+    for i in range(1, N*M//2+1):
+        for j in range(2):
+            a = random.randint(0, N-1)
+            b = random.randint(0, M-1)
+            while grid[a][b] != 0:
+                a = random.randint(0, N-1)
+                b = random.randint(0, M-1)
+            grid[a][b] = i
 
 #This function checks if given position is valid or not 
 def check_valid_position(i, j):
-	return 0 <= i < N and 0 <= j < N
+	return 0 <= i < N and 0 <= j < M
 
-#This function checks if the given position has a movement state or not
-def check_available_move(i, j):
-    return (check_valid_position(i-1, j) and check_empty(i-1, j)) or \
-           (check_valid_position(i+1, j) and check_empty(i+1, j)) or \
-           (check_valid_position(i, j-1) and check_empty(i, j-1)) or \
-           (check_valid_position(i, j+1) and check_empty(i, j+1))
+#This function checks if the given cells are identical cells or not
+def check_identical_cells(i1, j1, i2, j2):
+    return grid[i1][j1] == grid[i2][j2]
 
-#This function moves the position with the empty cell
-def move(i, j):
-    if check_valid_position(i-1, j) and check_empty(i-1, j): 
-        grid[i][j], grid[i-1][j] = grid[i-1][j], grid[i][j]
-    elif check_valid_position(i+1, j) and check_empty(i+1, j): 
-        grid[i][j], grid[i+1][j] = grid[i+1][j], grid[i][j]
-    elif check_valid_position(i, j-1) and check_empty(i, j-1): 
-        grid[i][j], grid[i][j-1] = grid[i][j-1], grid[i][j]
-    elif check_valid_position(i, j+1) and check_empty(i, j+1): 
-        grid[i][j], grid[i][j+1] = grid[i][j+1], grid[i][j]
+#This function shows the given cell
+def show(i, j):
+    viewed_grid[i][j] = 1
+
+#This function hides the given cell
+def hide(i, j):
+    viewed_grid[i][j] = 0
+
+#This function checks if the given position is hidden or not
+def check_hide_cell(i, j):
+    return viewed_grid[i][j] == 0
+
+#This function checks if the given positions are the same or not
+def check_same_position(i1, j1, i2, j2):
+    return i1 == i2 and j1 == j2
 
 #This function clears the game structures
 def grid_clear():
-	global grid
-	grid = [[0] * N for i in range(N)]
+    global grid, viewed_grid
+    grid = [[0] * M for i in range(N)]
+    viewed_grid = [[0] * M for i in range(N)]
 
-#This function reads a valid position
+#This function reads a valid input of cells
 def read_input():
-    i, j = map(int, input('Enter the row index and column index: ').split())
-    while not check_valid_position(i, j) or check_empty(i, j) or not check_available_move(i, j):
-        i, j = map(int, input('Enter a valid row index and a valid column index: ').split())
-    return i, j
+    i1, j1, i2, j2 = map(int, input('Enter the two positions of the cells: ').split())
+    while not check_valid_position(i1, j1) or not check_valid_position(i2, j2) or \
+          not check_hide_cell(i1, j1) or not check_hide_cell(i2, j2) or check_same_position(i1, j1, i2, j2):
+        i1, j1, i2, j2 = map(int, input('Enter a valid two positions of the cells: ').split())
+    return i1, j1, i2, j2
 
 
 #MAIN FUNCTION
 def play_game():
-    print("8 Puzzle Game!")
+    print("Remembery Game!")
     print("Welcome...")
     print("============================")
     while True:
         #Prints the grid
         print_grid()
         #Read an input from the player
-        i, j = read_input()
-        #Move with the input position
-        move(i, j)
+        i1, j1, i2, j2 = read_input()
+        #Show the input cells
+        show(i1, j1)
+        show(i2, j2)
+        #Prints the grid
+        print_grid()
+        #Check if the input cells are identical
+        if not check_identical_cells(i1, j1, i2, j2):
+            #Hide the input cells
+            hide(i1, j1)
+            hide(i2, j2)
+            print('The selected cells are not the same')
+        else:
+            print('The selected cells are the same')
         #Check if the grid has a win state
         if check_win():
             #Prints the grid
